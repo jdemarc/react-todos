910f54a79ea82f44b69b087a961854be27e8acb7
joseph <josephjohndemarco@gmail.com>
2020-12-05 09:45:23 -0500

    Style input box.

--- src/components/InputBar/index.jsx
+++ src/components/InputBar/index.jsx
@@ -4,7 +4,11 @@ const InputBar = ( { todo, setTodo, handleSubmit } ) => {
   
   return (
     <div className="input-box">
+        <button className="btn">
+
+        </button>
         <input type="text"
+          placeholder="Create a new todo..."
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
           onKeyPress={(e) => e.key === 'Enter' ? handleSubmit(e) : null}
